d4034d80e35bb4f0e03d2cc7d533122fe3b99c6a
lieselmatos <lieselmatos@gmail.com>
2026-02-28 18:59:39 -0300

    Add internal deque implementation

---
+++ deque.c
@@ -0,0 +1,31 @@
+// Internal deque implementation used by the palindrome program.
+// Provides a doubly linked deque storing characters.
+
+// INCLUDES ----------------------
+
+#include <stdio.h>
+#include <stdlib.h>
+
+// FOWARD DECLARATIONS ----------------------
+
+// Node of the deque (internal structure)
+struct deque_n;
+
+// TYPE DEFINITIONS ----------------------
+
+struct deque {
+    struct deque_n * b; // begginig
+    struct deque_n * e; // ending
+    int size;
+};
+
+struct deque_n {
+    struct deque_n * n; // next
+    struct deque_n * p; // previous
+    char c; // character
+};
+
+// INTERNAL HELPERS ----------------------
+
+// PUBLIC FUNCTION IMPLEMENTATIONS ----------------------
+
